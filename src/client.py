68e9a9573035afbc2375e830e917a50fa676a5eb
40D4Y5 <mohammed1alhaimi@gmail.com>
2021-02-20 00:45:22 +0800

    Update client.py
    dd

--- src/client.py
+++ src/client.py
@@ -2,3 +2,30 @@
 Using Python 3.8   
 Clinet side 
 """
+import socket
+import json
+import subprocess
+import time
+import os
+import threading
+import shutil
+import sys
+
+
+def connectToServer():
+    while True:
+
+        try:
+            SOK.connect((IP, PORT))
+            print('connected')
+            break
+        except:
+            time.sleep(9)
+            connectToServer()
+
+
+IP = '192.168.1.2'
+PORT = 55555
+SOK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connectToServer()
+SOK.close()
