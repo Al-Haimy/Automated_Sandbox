9c032b91b7c73168cdf756e46bfdfeb0c5b751e4
40D4Y5 <mohammed1alhaimi@gmail.com>
2021-02-22 16:49:27 +0800

    Update client.py

--- src/client.py
+++ src/client.py
@@ -16,16 +16,20 @@ def connectToServer():
     while True:
 
         try:
+            print("trying to find connection")
             SOK.connect((IP, PORT))
             print('connected')
             break
         except:
+            print("Error")
             time.sleep(9)
             connectToServer()
 
 
 IP = '192.168.1.2'
 PORT = 55555
+print(f'just assigned Ip {IP} and Port {PORT}')
 SOK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(f'Establishing Connection')
 connectToServer()
 SOK.close()
