86877cf04ca23923fedf4a266b0dbe64f1a11dbd
40D4Y5 <mohammed1alhaimi@gmail.com>
2021-02-28 23:40:19 +0800

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,6 +1,6 @@
 """
-Using Python 3.8   
-Clinet side 
+Using Python 3.8
+Clinet side
 """
 import socket
 import json
@@ -12,6 +12,30 @@ import shutil
 import sys
 
 
+def recv_data():
+    data = ''
+    while True:
+        try:
+            data = data + SOK.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+
+def _check():
+    ls = os.listdir(PATH)
+    return ls
+
+
+def share_file():
+    while True:
+        con = recv_data()
+        if con == "check":
+            folders = []
+            folders = _check()
+            send_data(folders)
+
+
 def connectToServer():
     while True:
 
@@ -19,11 +43,12 @@ def connectToServer():
             # print("trying to find connection")
             SOK.connect((IP, PORT))
             print('connected')
+            share_file()
             break
         except:
             # print("Error")
             time.sleep(9)
-            connectToServer()
+            # connectToServer()
 
 
 IP = '192.168.1.2'
@@ -33,3 +58,4 @@ SOK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(f'Establishing Connection')
 connectToServer()
 SOK.close()
+PATH = r'\download\'
